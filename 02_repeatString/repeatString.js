881f8a8272c9fa51f2015ff4f574c62793ec8556
Ruby Casey <ruby@cxsey.com>
2024-04-14 01:48:51 -0400

    Repeating String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+	let outputString = "";
+	if (count < 0) {
+		return "ERROR";
+	} else if (count == 0) {
+		return "";
+	}
+	for (let i = 0; i < count; i++){
+		outputString = outputString.concat(string);
+	};
+	return outputString;
 };
 
 // Do not edit below this line
