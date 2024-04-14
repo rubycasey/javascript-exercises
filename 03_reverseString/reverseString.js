e242cbbe0c51f2ab4d1573268dc4d20776b6468b
Ruby Casey <ruby@cxsey.com>
2024-04-14 01:59:29 -0400

    Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let stringSplit = [];
+	let stringConcat = "";
+	for (let i = 0; i < string.length; i++) {
+		// splits each letter into it's own array index
+		stringSplit.push(string[i]);
+	};
+	for (let i = string.length - 1; i > -1; i--) {
+		// reassembles split array into reversed string
+		stringConcat = stringConcat.concat(stringSplit[i]);
+	}
+	return stringConcat;
 };
 
 // Do not edit below this line
