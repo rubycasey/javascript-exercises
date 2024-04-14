b59f77397e351ce4f012a43a8302283bd21acb69
Ruby Casey <ruby@cxsey.com>
2024-04-14 14:55:27 -0400

    Remove From Array (Full)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(arr, ...values) {
 	let newArray = arr;	
 	for (let i = 0; i < values.length; i++) {
-		newArray = newArray.filter((item) => item != values[i]);
+		newArray = newArray.filter((item) => item !== values[i]);
 	};
 	return newArray;
 };
