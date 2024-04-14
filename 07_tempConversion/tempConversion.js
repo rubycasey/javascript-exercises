3612ac0f21b81af65d5252ba880efd69fde44fcf
Ruby Casey <ruby@cxsey.com>
2024-04-14 15:15:04 -0400

    Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+	let celcius = Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
+	return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+	let fahrenheit = Math.round((celcius * (9/5) + 32) * 10) / 10;
+	return fahrenheit;
 };
 
 // Do not edit below this line
