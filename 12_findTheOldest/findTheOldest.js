621b381a2d8f2f72d21a516cc1654b01831820cc
Ruby Casey <ruby@cxsey.com>
2024-06-27 17:53:13 -0400

    Find The Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	const oldest = people.sort((a, b) => {
+		if (!("yearOfDeath" in b)) {
+			b.yearOfDeath = new Date().getFullYear();
+		};
+		const prevPerson = a.yearOfDeath - a.yearOfBirth;
+		const nextPerson = b.yearOfDeath - b.yearOfBirth;
+		return prevPerson > nextPerson ? -1 : 1;
+	})
+	return oldest[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
