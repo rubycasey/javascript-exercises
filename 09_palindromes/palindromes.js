ec124f788a805ca7d0413c8e8278d16206f88a96
Ruby Casey <ruby@cxsey.com>
2024-06-27 17:12:12 -0400

    Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (inputRaw) {
+	// Formats input
+	input = inputRaw.toLowerCase().replace(/[\.,?!]/g, "").replace(/\s+/g, "");
+	
+	// Creates array for first half of characters
+	let array = [];
+	for (let i = 0; i < (input.length / 2); i++) {
+		array.push(input.charAt(i));
+	};
 
+	// Flips array
+	arrayRev = [...array];
+	arrayRev.reverse();
+	
+	// Creates string out of array
+	let arrayString = "";
+	arrayRev.forEach((char) => {
+		arrayString = arrayString.concat(char);
+	});
+
+	// Compares reversed half to second half of input string
+	if ((input.slice(input.length / 2, input.length)) == arrayString) {
+		return true;
+	} else {
+		return false;
+	};
 };
 
 // Do not edit below this line
