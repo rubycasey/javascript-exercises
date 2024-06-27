012df15e54286fe312d658cca2a4aaa40f9c6a13
Ruby Casey <ruby@cxsey.com>
2024-06-27 17:24:07 -0400

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	let input = Number(n);
+	if (input == 0) {
+		return 0;
+	} else if (input < 0) {
+		return "OOPS";
+	};
+	let lastNumber = 1;
+	let secondLastNumber = 0;
+	let currentNumber = 1;
+	for (let i = 1; i < input; i++) {
+		currentNumber = secondLastNumber + lastNumber
+		secondLastNumber = lastNumber;
+		lastNumber = currentNumber;
+	};
+	return currentNumber;
 };
 
 // Do not edit below this line
