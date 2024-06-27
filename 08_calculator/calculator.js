e6c94a5c64bdf572f1053a68b21313ab0ad0cdd4
Ruby Casey <ruby@cxsey.com>
2024-06-27 16:09:22 -0400

    Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+		return 1;
+	} else {
+		let lastNumber = 0;
+		let output = 0;
+		for (let i = 1; i < a+1; i++) {
+			if (lastNumber == 0) {
+				lastNumber = 1;
+			}			
+			output = lastNumber * i;
+			lastNumber = output;
+		}
+		return output;
+	};
 };
 
 // Do not edit below this line
